65b461b3ae6a31e94fd7e2268a6cebb46706d9a7
Sahil <ppapalkar.sahil@gmail.com>
2020-11-02 20:27:27 -0500

    Change all char to lower case

--- src/trie/trie.c
+++ src/trie/trie.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<ctype.h>
 #include "trie.h"
-
+// Initializes the trienode
 trienode* init_trienode(char ch){
     trienode *node = (trienode*) malloc(sizeof(trienode));
     node->ch = ch;
@@ -9,14 +10,19 @@ trienode* init_trienode(char ch){
     node->freq = 0;
 }
 
+// Inserts a node to trie
 void insert(trienode *root, char str[], int freq){
     trienode *current, *child;
     int index = 0;
     current = root;
-
+    // Iterate through each character and add it to trie
     while(str[index] != '\0'){
+        str[index] = tolower(str[index]);
+        // Check if character is a-z or ' ' 
+        if((str[index] < 97 || str[index] > 122) && str[index] != ' ')
+            continue;
+        // Get the already existing node for the chracter else create new node
         child = (trienode*) get(current->children, str[index]);
-
         if(child == NULL){
             child = init_trienode(str[index]);
             put(current->children, str[index], child);
@@ -25,6 +31,7 @@ void insert(trienode *root, char str[], int freq){
         current = child;
         index+=1; 
     }
+    // Last node will be '*' and frequency
     child = init_trienode('*');
     child->freq = freq;
     put(current->children, '*', child);
