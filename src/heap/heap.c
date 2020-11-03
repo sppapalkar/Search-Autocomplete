3e3e0ab8f859dd679601ee0f86ba6c9d3fb15b4c
Sahil <ppapalkar.sahil@gmail.com>
2020-11-02 19:41:10 -0500

    Add comments to heap file

--- src/heap/heap.c
+++ src/heap/heap.c
@@ -3,6 +3,7 @@
 #include<limits.h>
 #include "heap.h"
 
+// Initialized min heap 
 heap* init_heap(int size){
     heap *minheap = (heap*) malloc(sizeof(heap));
     minheap->elements = (heapnode*) calloc(size+1, sizeof(heapnode));
@@ -10,6 +11,7 @@ heap* init_heap(int size){
     return minheap;
 }
 
+// Swap two elements of min heap
 void swap(heap *minheap, int index1, int index2){
     heapnode temp;
     temp = minheap->elements[index1];
@@ -17,39 +19,45 @@ void swap(heap *minheap, int index1, int index2){
     minheap->elements[index2] = temp; 
 }
 
+// Heapify the heap - least node becomes the root
+// Recursively compares a node with its childrens and swap so that the smallest amongst the 3 is the parent 
 void heapify(heap *minheap, int element){
     int left, right, smallest;
     left = 2*element;
     right = (2*element) + 1;
     smallest = element;
-
+    // Compare with left and child
     if(left <= minheap->size && minheap->elements[left].val < minheap->elements[smallest].val){
         smallest = left;
     }
     if(right <= minheap->size && minheap->elements[right].val < minheap->elements[smallest].val){
         smallest = right;
     }
+    // If the smallest element is not the parent then swap the smallest child with parent
     if(smallest != element){
         swap(minheap, smallest, element);
         heapify(minheap, smallest);
     }
 }
-
+// Add a new value to the heap
 void offer(heap *minheap, char str[], int val){
     int index;
+    // Add new element to the end of heap
     minheap->size++;
     minheap->elements[minheap->size].str = str;
     minheap->elements[minheap->size].val = val;
-
+    
+    // Move the node up the tree based on its value
     index = minheap->size;
     while(minheap->elements[index/2].val > val){
         swap(minheap, index, index/2);
         index = index/2;
     }
 }
-
+// Poll the tree to remove the smallest value
 char* poll(heap *minheap){
     char *min = minheap->elements[0].str;
+    // Replace the last element with first and then call heapify
     minheap->elements[0] = minheap->elements[minheap->size];
     minheap->size--;
     heapify(minheap, 0);
